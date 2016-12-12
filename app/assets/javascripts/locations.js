3a319aca6b510e868395712010c495ea1803d84f
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-12-11 19:29:31 -0800

    Update svg; add hover stroke color

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,3 +1,8 @@
-$('.skylineSF').on('click', function() {
-  $('svg').fadeOut();
-})
+$(document).ready(function() {
+    $("a").mouseenter(function() {
+        $(this).find("line").attr("stroke", "maroon");
+     });
+    $("a").mouseleave(function() {
+        $(this).find("line").attr("stroke", "#fff");
+     });
+});
